07d18298739a5ef72cb84746c21439d45641f0f7
Ehtasham Munib <emunib@outlook.com>
2022-02-10 11:46:34 -0600

    add matched animation

--- js/script.js
+++ js/script.js
@@ -7,18 +7,23 @@ let resetTimeoutId = "";
 
 function card(value) {
   const card = document.createElement("div");
+  const bg = document.createElement("div");
   const flipper = document.createElement("div");
   const front = document.createElement("div");
   const back = document.createElement("div");
 
   card.classList.add("card");
+  bg.classList.add("card__bg");
   flipper.classList.add("card__flipper");
   front.classList.add("card__front");
   back.classList.add("card__back");
 
+  bg.innerText = value;
   back.innerText = value;
+  card.setAttribute("data-id", value);
 
   flipper.append(front, back);
+  card.append(bg);
   card.append(flipper);
 
   card.addEventListener("click", () => {
@@ -28,26 +33,36 @@ function card(value) {
   return card;
 }
 
+function clear() {
+  cards.forEach(card => {if (!card.classList.contains('matched')) card.classList.remove("open", "shake")});
+}
 function reset() {
-  cards.forEach((card) => card.classList.remove("open", "shake"));
+  cards.forEach(card => card.classList.remove("open", "shake", "matched"));
 }
 
 function getOpenCards() {
-  return cards.filter((card) => card.classList.contains("open"));
+  return cards.filter((card) => card.classList.contains("open") && !card.classList.contains("matched"));
 }
 
 function openCard(card) {
   if (getOpenCards().length < 2) {
     clearTimeout(resetTimeoutId);
-    resetTimeoutId = setTimeout(reset, 1500);
+    resetTimeoutId = setTimeout(clear, 1500);
     card.classList.add("open");
 
-    const openCards = getOpenCards()
+    const openCards = getOpenCards();
     if (openCards.length === 2) {
-      setTimeout(() => {
-        openCards.forEach(card => card.classList.add('shake'))
-      }, 1000)
-      
+      if (
+        openCards[0].getAttribute("data-id") ===
+        openCards[1].getAttribute("data-id")
+      ) {
+        clearTimeout(resetTimeoutId);
+        openCards.forEach((card) => card.classList.add("matched"));
+      } else {
+        setTimeout(() => {
+          openCards.forEach((card) => card.classList.add("shake"));
+        }, 1000);
+      }
     }
   }
 }
